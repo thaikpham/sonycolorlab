ffe523fbd97be3b5a0a975bfe22e862f409c75ab
thaikpham <thaikpham.art@gmail.com>
2025-07-23 19:27:22 +0700

    Update recipes.js

--- recipes.js
+++ recipes.js
@@ -353,7 +353,7 @@ const recipesData = [
         "contrast": "normal",
         "saturation": "maximum",
         "tags": ["color-explosion", "maximum-saturation", "super-vibrant", "high-energy", "bold-statements", "playful-side"],
-        "whiteBalance": "4000K, A7-M1",
+        "whiteBalance": "3700K, A5-M1",
         "settings": { "Black level": "+2", "Gamma": "Still", "Black Gamma": "Middle +7", "Knee": "Manual 75% +5", "Color Mode": "S-Gamut3", "Saturation": "+20", "Color Phase": "-1" },
         "colorDepth": { "R": "+2", "G": "+7", "B": "+7", "C": "+7", "M": "+7", "Y": "+4" },
         "detailSettings": { "Level": "0" },
