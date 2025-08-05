0215ff266d9f94cea74c1d3924383e7cc630914e
thaikpham <thaikpham.art@gmail.com>
2025-08-05 20:09:48 +0700

    Update recipes.js

--- recipes.js
+++ recipes.js
@@ -940,6 +940,7 @@ const recipesData = [
             "https://placehold.co/800x600/708090/ffffff?text=Ortho+Portrait+3"
         ]
     },
+    {
             "id": "PROCOLOR-001",
         "name": { "vi": "PROCOLOR-001: ClearCast Pro", "en": "PRO-SCL-001: ClearCast Pro" },
         "formattedName": "PRO-SCL-001: ClearCast Pro",
