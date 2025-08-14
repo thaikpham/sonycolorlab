4c21d303a7b5a46f58dfa7d42dd4859db3009046
thaikpham <thaikpham.art@gmail.com>
2025-08-14 20:16:01 +0700

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ import { getAuth, signInAnonymously } from "https://www.gstatic.com/firebasejs/1
 import { getFirestore, doc, getDoc } from "https://www.gstatic.com/firebasejs/11.6.1/firebase-firestore.js";
 
 // --- PDF & Canvas Library Imports ---
-// We will load these dynamically when needed to keep initial load fast
+// We will load these dynamically when needed to keep initial load fast 
 const JSPDF_URL = "https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.5.1/jspdf.umd.min.js";
 const HTML2CANVAS_URL = "https://cdnjs.cloudflare.com/ajax/libs/html2canvas/1.4.1/html2canvas.min.js";
 
