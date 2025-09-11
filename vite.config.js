411d7df8e615e3e8de05dd3fc3de153df082a661
thaikpham <thaikpham.art@gmail.com>
2025-09-11 20:42:15 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,5 @@ import { defineConfig } from 'vite';
 export default defineConfig({
   build: {
     outDir: 'dist',
-    emptyOutDir: false // prevent Vite from deleting files your build.js puts in dist/
   }
 });
