ca6503c3c3839337f477ee1fa8eee9b20177e8ba
thaikpham <thaikpham.art@gmail.com>
2025-09-11 20:50:38 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,8 @@
 import { defineConfig, loadEnv } from 'vite';
 
-export default defineConfig(const env = loadEnv(mode, process.cwd(), 'VITE_');
+export default ({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), 'VITE_');
   console.log('VITE env variables:', env);
 
-  return defineConfig({}););
+  return defineConfig({});
+};
