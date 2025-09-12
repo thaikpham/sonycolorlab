6a7c9dcdc37382c27c64c8b1342ad19b1ceefcf7
thaikpham <thaikpham.art@gmail.com>
2025-09-12 15:07:10 +0700

    Update ui.js

--- src/services/ui.js
+++ src/services/ui.js
@@ -370,10 +370,6 @@ export function renderHeader() {
                 <svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5" viewBox="0 0 48 48"><path fill="#FFC107" d="M43.611 20.083H42V20H24v8h11.303c-1.649 4.657-6.08 8-11.303 8c-6.627 0-12-5.373-12-12s5.373-12 12-12c3.059 0 5.842 1.154 7.961 3.039l5.657-5.657C34.046 6.053 29.268 4 24 4C12.955 4 4 12.955 4 24s8.955 20 20 20s20-8.955 20-20c0-1.341-.138-2.65-.389-3.917z"></path><path fill="#FF3D00" d="m6.306 14.691l6.571 4.819C14.655 15.108 18.961 12 24 12c3.059 0 5.842 1.154 7.961 3.039l5.657-5.657C34.046 6.053 29.268 4 24 4C16.318 4 9.656 8.337 6.306 14.691z"></path><path fill="#4CAF50" d="M24 44c5.166 0 9.86-1.977 13.409-5.192l-6.19-5.238A11.91 11.91 0 0 1 24 36c-5.202 0-9.619-3.317-11.283-7.946l-6.522 5.025C9.505 39.556 16.227 44 24 44z"></path><path fill="#1976D2" d="M43.611 20.083H42V20H24v8h11.303c-.792 2.237-2.231 4.166-4.087 5.571l6.19 5.238C42.012 35.245 44 30.028 44 24c0-1.341-.138-2.65-.389-3.917z"></path></svg>
                 <span class="hidden sm:inline" data-translate-key="signInBtn"></span>
             </button>
-             <button id="signInFacebookBtn" class="btn bg-blue-600 hover:bg-blue-700 text-white py-2 px-4">
-                 <svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5" viewBox="0 0 24 24" fill="currentColor"><path d="M12 2.04c-5.52 0-10 4.48-10 10s4.48 10 10 10s10-4.48 10-10s-4.48-10-10-10zm2.25 10.28h-1.68v4.64h-2.1v-4.64h-1.12v-1.87h1.12v-1.26c0-1.2.56-1.87 1.87-1.87h1.4v1.87h-.84c-.35 0-.42.18-.42.42v.84h1.26l-.14 1.87z"></path></svg>
-                 <span class="hidden sm:inline" data-translate-key="signInMeta"></span>
-            </button>
         </div>
     `;
 
